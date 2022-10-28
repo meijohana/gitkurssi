a71a354a774056605d022dc522b5d432690c67fe
meijohana <aojohanna@gmail.com>
2022-10-28 14:27:43 +0300

    reset

--- hello_world.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-# Ensimmäinen ohjelma
-# Tekijä: Katja Heiskanen, keskusta
-# Opiskelijanumero: K444338
-
-print("Hello World!")
-=======
-print("Hello world!")
->>>>>>> branches/master
